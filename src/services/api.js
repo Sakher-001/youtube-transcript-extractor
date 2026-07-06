e54fe360b747e61c5ed1460c57acb07cbe18a5f9
Sa5oor <sakherclash777@gmail.com>
2026-07-06 22:24:24 +0300

    Update API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const getTranscript = async (url) => {
